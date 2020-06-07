c727ec93ab118b8bd8a0531fda0f935a031c4348
ShrutiNadkarni <shrutinadkarni1698@gmail.com>
2020-06-07 09:42:17 +0530

    Update login.component.ts

--- frontend/frontend/src/app/login/login.component.ts
+++ frontend/frontend/src/app/login/login.component.ts
@@ -30,16 +30,16 @@ export class LoginComponent implements OnInit {
       // this.password="admin2";
      
     
-    if(this.email == "tmcadmin@live.com" && this.password=="admin1" )
-      {
-        console.log("Inside Login User");
-        this.router.navigate(['slandingpg']);
-        console.log("Welcome ");
-      }
-      else{
-        window.alert("Invalid Email or Password !!!! Please try again");
+//     if(this.email == "tmcadmin@live.com" && this.password=="admin1" )
+//       {
+//         console.log("Inside Login User");
+//         this.router.navigate(['slandingpg']);
+//         console.log("Welcome ");
+//       }
+//       else{
+//         window.alert("Invalid Email or Password !!!! Please try again");
        
-      }
+//       }
       //   // this.auth.login(this.email, this.password)
       //   // .pipe(first())
       //   //  .subscribe(
@@ -47,7 +47,15 @@ export class LoginComponent implements OnInit {
       //   // err => this.error = 'Could not authenticate'
       // );
 
-
+       this.http.post('http://localhost:8080/login',{ // data is sent to backend api with email and password entered by the user
+      email:loginForm.form.value.email,
+      password:loginForm.form.value.password
+    }).subscribe((result:any)=>{
+      if(result == 'success'){
+        window.location.href ="http://localhost:8080/slandingpg"; // if auth returns success user is logged in and redirected to the settings landing pagr
+      }
+        
+    })
 
 
 
